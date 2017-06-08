3050f67d75f63c8fb3d97658e50a05fab1085777
raholm <rasmus.holm723@gmail.com>
2017-06-08 02:18:52 +0200

    Added cluster analyses.

--- dota2/src/analysis/cluster.py
+++ dota2/src/analysis/cluster.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 import pandas as pd
 
 from src.analysis.data import get_team_compositions_by_name, get_picks_bans_international2015, \
-    get_team_compositions_by_id, get_team_compositions_against
+    get_team_compositions_by_id, get_team_compositions_against, get_picks_bans_major_events, get_team_compositions_with
 from src.database.heroes import Heroes
 from src.resources.ROCK.data_point import DataPoint
 from src.resources.ROCK.rock_algorithm import RockAlgorithm
@@ -13,6 +13,8 @@ from src.resources.ROCK.rock_algorithm import RockAlgorithm
 def get_rock_clusters(data, min_clusters, threshold):
     if isinstance(data, pd.DataFrame):
         data = data.values.tolist()
+
+    data = [sorted(d) for d in data]
 
     points = [DataPoint(i, d)
               for i, d in enumerate(data)]
@@ -49,9 +51,8 @@ def print_rock_clusters(clusters, **kwargs):
         print("Cluster %i\n%s" % (cluster_id, get_team_compositions_by_name(cluster, **kwargs),))
 
 
-def summarize_rock_clusters(clusters, **kwargs):
+def summarize_rock_clusters(clusters, min_freq, **kwargs):
     clusters_dict = {}
-    min_frequency = 2
 
     for cluster_id, cluster in clusters.items():
         readable_cluster = get_team_compositions_by_name(cluster, **kwargs)
@@ -64,7 +65,7 @@ def summarize_rock_clusters(clusters, **kwargs):
                 cluster_dict[hero] += 1
 
         for key, val in cluster_dict.items():
-            if val >= min_frequency:
+            if val >= min_freq:
                 clusters_dict[cluster_id][key] = val
 
     print(json.dumps(clusters_dict, indent=4, sort_keys=True))
@@ -79,32 +80,45 @@ def cl_major_events():
 
 
 def cl_all_against_antimage():
-    # Check antimage order count
     heroes = Heroes()
-    # print(heroes.heroes)
 
-    picks_bans = get_picks_bans_international2015()
+    picks_bans = get_picks_bans_major_events()
     team_comps = get_team_compositions_by_id(picks_bans)
 
     hero_id = heroes.inverse["Anti-Mage"]
     team_comps_against = get_team_compositions_against(team_comps, hero_id)
     print(heroes.heroes[hero_id])
+    print(team_comps_against.shape)
 
-    min_clusters = 2
-    threshold = 0.3
+    min_clusters = 15
+    threshold = 0.25
+    min_freq = 10
     clusters = get_rock_clusters(team_comps_against, min_clusters, threshold)
     # print_rock_clusters(clusters, heroes=heroes)
-    summarize_rock_clusters(clusters, heroes=heroes)
+    summarize_rock_clusters(clusters, min_freq, heroes=heroes)
 
 
 def cl_all_with_wisp():
-    # Check wisp order count
-    pass
+    heroes = Heroes()
+
+    picks_bans = get_picks_bans_major_events()
+    team_comps = get_team_compositions_by_id(picks_bans)
+
+    hero_id = heroes.inverse["Io"]
+    team_comps_against = get_team_compositions_with(team_comps, hero_id)
+    print(heroes.heroes[hero_id])
+
+    min_clusters = 2
+    threshold = 0.3
+    min_freq = 4
+    clusters = get_rock_clusters(team_comps_against, min_clusters, threshold)
+    # print_rock_clusters(clusters, heroes=heroes)
+    summarize_rock_clusters(clusters, min_freq, heroes=heroes)
 
 
 def main():
     # Sort rows before clustering
-    pass
+    cl_all_against_antimage()
 
 
 if __name__ == '__main__':
